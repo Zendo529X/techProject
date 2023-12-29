22b53ff253112df969ddcee429215b19bccf06b9
shengche <shengche.chiu@wiadvance.com>
2023-12-29 16:31:12 +0800

    add create user api

--- Controller/UserController.py
+++ Controller/UserController.py
@@ -4,25 +4,32 @@
 # @Site :
 # @File : UserController.py
 # @Software: Pycharm
+from flask import request
 from flask_restx import Namespace, Resource, fields
-from Service.UserService import UserService
+from Service.UserService import *
 from model.Users import Users
 
 api = Namespace('user', description='user Data')
 
+user_model = api.model('user_model', {
+    'id': fields.Integer(required=False, description="id", help="id can be blank."),
+    'name': fields.String(required=True, description="name", help="name cannot be blank."),
+    'email': fields.String(required=True, description="email", help="email cannot be blank."),
+    'cell_phone': fields.String(required=True, description="cell_phone", help="cell_phone cannot be blank.")
+})
 
+userService = UserService()
 
 @api.route('', methods=["GET", "POST", "DELETE", "PUT"])
 class UserController(Resource):
-    @api.route("/")
-    class createUser(Resource):
-        def post(self):
-            return
 
-    @api.route("/")
-    class getUser(Resource):
-        def get(self):
-            return UserService.getUser(self)
+    def get(self):
+        return userService.getUser()
+
+        # return 'hello get user'
 
 
-            # return 'hello get user'
+    @api.expect(user_model)
+    def post(self):
+        data = request.get_json()
+        return userService.createUser(data)
--- Service/UserService.py
+++ Service/UserService.py
@@ -7,6 +7,7 @@
 
 from model.Users import Users
 
+
 # todo add user service
 
 class UserService:
@@ -14,7 +15,17 @@ class UserService:
         users = Users.query.all()
         return users
 
-    def createUser(self):
+    def createUser(self, data):
+        id = data.get('id',None)
+        name = data.get('name',None)
+        email = data.get('email',None)
+        cell_phone=data.get('cell_phone',None)
+        user = Users()
+        user.id = id
+        user.name=name
+        user.email =email
+        user.cell_phone = cell_phone
+        user.create(user)
         return
 
     def updateUser(self):
